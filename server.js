d9926b75314e3493b24779f54adf3f53a5f654d4
Alejandro Cosa Liñán <alejandro.cosa.linan@gmail.com>
2020-06-01 09:23:38 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+function onRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.render('index.html')
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
+
